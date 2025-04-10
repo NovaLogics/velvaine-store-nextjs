ad0494287a80db17827f48da156cb87c1445e98b
NovaLogics <rajitha.shavinda@gmail.com>
2025-04-10 18:56:18 +0530

    feat(home-collection): ui and api functions

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -7,9 +7,10 @@ const Collections = async () => {
   const collections = await getCollections();
 
   return (
-    <div>
+    <div className="flex flex-col items-center gap-10 py-8 px-5">
       <p className="text-heading1-bold">Collections</p>
-      <div>
+
+      <div className="flex items-center justify-center gap-8">
         {collections.map((collection: CollectionType) => (
           <Link key={collection._id} href={`/collections/${collection._id}`}>
             <Image
