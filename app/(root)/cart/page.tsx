ae41a719dfae925bb15c3b33960a5d348c2383c7
NovaLogics <rajitha.shavinda@gmail.com>
2025-04-11 18:04:44 +0530

    feat(cart-page): impl cart page(only for large screens)

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -1,9 +1,95 @@
-import React from 'react'
+"use client";
+import useCart from "@/lib/hooks/useCart";
+import { MinusCircle, PlusCircle, Trash } from "lucide-react";
+import Image from "next/image";
+import React from "react";
 
 const Cart = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const cart = useCart();
 
-export default Cart
+  const total = cart.cartItems.reduce(
+    (account, cartItem) => account + cartItem.item.price * cartItem.quantity,
+    0
+  );
+
+  const totalRounded = parseFloat(total.toFixed(2));
+
+  return (
+    <div className="bg-white text-black flex gap-20 py-16 px-10">
+      <div className="w-2/3">
+        <p className="text-heading3-bold">Shopping Cart</p>
+        <hr className="my-6" />
+
+        {cart.cartItems?.length === 0 ? (
+          <p className="text-body-bold">No items in cart!</p>
+        ) : (
+          <div>
+            {cart.cartItems.map((cartItem) => (
+              <div className="w-full flex hover:bg-grey-2 px-6 py-5 items-center justify-between">
+                <div className="flex items-center">
+                  <Image
+                    src={cartItem.item.media[0]}
+                    className="rounded-lg w-32 h-32 object-cover "
+                    alt="product"
+                    width={100}
+                    height={100}
+                  />
+                  <div className="flex flex-col gap-3 ml-4">
+                    <p className="text-body-bold">{cartItem.item.title}</p>
+                    {cartItem.color && (
+                      <p className="text-small-medium">{cartItem.color}</p>
+                    )}
+                    {cartItem.size && (
+                      <p className="text-small-medium">{cartItem.size}</p>
+                    )}
+                    <p className="text-small-medium">${cartItem.item.price}</p>
+                  </div>
+                </div>
+
+                <div className="flex gap-4 items-center">
+                  <MinusCircle
+                    className="hover:text-red-1 cursor-pointer"
+                    onClick={() => {
+                      cart.decreaseQuantity(cartItem.item._id);
+                    }}
+                  />
+                  <p className="text-body-bold">{cartItem.quantity}</p>
+                  <PlusCircle
+                    className="hover:text-red-1 cursor-pointer"
+                    onClick={() => {
+                      cart.increseQuantity(cartItem.item._id);
+                    }}
+                  />
+                </div>
+
+                <Trash
+                  className="hover:text-red-1 cursor-pointer"
+                  onClick={() => cart.removeItem(cartItem.item._id)}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="w-1/3 flex flex-col gap-8 bg-grey-2 rounded-lg px-4 py-5">
+        <p className="text-heading4-bold pb-4">
+          Summary{" "}
+          <span>{`(${cart.cartItems.length} ${
+            cart.cartItems.length > 1 ? "items" : "item"
+          })`}</span>
+        </p>
+        <div className="flex justify-between text-body-semibold">
+          <span>Total Amount</span>
+          <span>${totalRounded}</span>
+        </div>
+
+        <button className="border rounded-lg text-body-bold bg-white mt-5 py-1 w-full hover:bg-black hover:text-white">
+          Proced to Checkout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
