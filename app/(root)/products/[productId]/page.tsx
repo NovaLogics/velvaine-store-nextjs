486585660e2dbc43d08caadcdc8d733f62d130d1
NovaLogics <rajitha.shavinda@gmail.com>
2025-04-11 16:10:39 +0530

    feat(product-details): impl ui functions

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -1,4 +1,5 @@
 import Gallery from "@/components/Gallery";
+import ProductInfo from "@/components/ProductInfo";
 import { getProductDetails } from "@/lib/actions";
 import React from "react";
 
@@ -14,7 +15,8 @@ const ProductDetails = async ({
 
   return (
     <div className="bg-white text-black flex justify-center items-start gap-16 py-10 px-5 max-md:flex-col max-md:items-center">
-      <Gallery productMedia={productDetails.media}/>
+      <Gallery productMedia={productDetails.media} />
+      <ProductInfo productInfo={productDetails}/>
     </div>
   );
 };
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,63 +1,14 @@
 "use client";
-import { useUser } from "@clerk/nextjs";
-import { Heart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import LikeHeart from "./LikeHeart";
 
 interface ProductCardProps {
   product: ProductType;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-  const { user } = useUser();
-  const router = useRouter();
-
-  const [loading, setLoading] = useState(false);
-  const [signedInUser, setSignedInUser] = useState<UserType | null>(null);
-  const [isLiked, setIsLiked] = useState(false);
-
-  const getUser = async () => {
-    try {
-      setLoading(true);
-      const response = await fetch("api/users");
-      const data = await response.json();
-      setSignedInUser(data);
-      setIsLiked(data.wishlist?.includes(product._id));
-      setLoading(false);
-    } catch (error) {
-      console.log("[user_GET]", error);
-    }
-  };
-
-  useEffect(() => {
-    if (user) getUser();
-  }, [user]);
-
-  const handleLike = async (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    event.preventDefault();
-
-    try {
-      if (!user) {
-        router.push("/sign-in");
-        return;
-      }
-
-      setLoading(true);
-      const response = await fetch("api/users/wishlist", {
-        method: "POST",
-        body: JSON.stringify({ productId: product._id }),
-      });
-      const updateUser = await response.json();
-      setSignedInUser(updateUser);
-      setIsLiked(updateUser.wishlist.includes(product._id));
-    } catch (error) {
-      console.log("[wishlist_POST]", error);
-    }
-  };
 
   return (
     <Link
@@ -79,9 +30,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
 
       <div className="flex justify-between items-center">
         <p className="text-base-medium font-semibold">${product.price}</p>
-        <button onClick={handleLike}>
-          <Heart fill={`${isLiked ? "red" : "white"}`} />
-        </button>
+        <LikeHeart product={product} />
       </div>
     </Link>
   );
